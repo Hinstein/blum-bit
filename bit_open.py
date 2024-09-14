21beb81b04fb475d4b9885b4a84b4b1948b3260c
hinsetin <0xhinstein@gmail.com>
2024-09-15 00:32:11 +0800

    更新init方法

--- bit_open.py
+++ bit_open.py
@@ -1,3 +1,6 @@
+from concurrent.futures import ThreadPoolExecutor
+
+import numpy as np
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
@@ -8,6 +11,66 @@ from kill_bit import terminate_processes
 from log_config import setup_logger
 
 logger = setup_logger('bit_blum', 'blum_auto.log')
+
+
+def create_threads(n, numbers):
+    """
+    创建 n 个线程，并平分随机顺序的数字给这些线程打印
+
+    :param n: 线程数量
+    :param bit_num_start: 数字范围的起始值
+    :param bit_num_end: 数字范围的结束值
+    :param error_list: 错误列表，如果不为空，则用该列表的值代替正常的数字范围
+    """
+
+    selected_values = get_file.get_id_by_seq(numbers)
+
+    logger.info("Original Dictionary:", selected_values)
+    result = np.array_split(numbers, n)
+    list_result = [subarray.tolist() for subarray in result]
+
+    # 使用 ThreadPoolExecutor 进行多线程处理
+    with ThreadPoolExecutor(max_workers=n) as executor:
+        futures = []
+        for i in range(n):
+            thread_name = f'Thread-{i + 1}'
+            future = executor.submit(print_numbers, list_result[i], thread_name, selected_values)
+            futures.append(future)
+
+            # 添加启动延迟
+            time.sleep(5)
+
+        logger.info("All task has completed")
+
+        # 等待所有线程完成并检查异常
+        for i, future in enumerate(futures):
+            try:
+                future.result()  # 捕获异常
+            except Exception as e:
+                logger.error(f"Thread-{i + 1} 发生异常: {e}")
+
+
+def print_numbers(numbers, thread_name, shuffled_dict):
+    """
+    打印给定的数字列表
+
+    :param numbers: 数字列表
+    :param thread_name: 线程名称
+    """
+    error = []
+
+    for num in numbers:
+        error_num = None
+        try:
+            # 获取指定序号的数据
+            item = shuffled_dict.get(num)
+            logger.info(f'{thread_name} 开始 {num} 任务 {item}')
+            error_num = execute_tasks(num, item)
+            logger.info(f'{thread_name} 结束 {num} 任务 {item}')
+        except Exception as e:
+            logger.error(f'{thread_name} 执行 {num} 任务报错 {item}')
+        if (error_num != None):
+            error.append(error_num)
 
 
 # Function to execute tasks for each profile
@@ -40,18 +103,12 @@ def execute_tasks(seq, id):
 
 
 if __name__ == '__main__':
-    # select = list(range(1, 41))
-    select = [1494]
-    selected_values = get_file.get_id_by_seq(select)
-    # Iterate through each profile directory
-    for key in selected_values:
-        seq = {key}
-        id = selected_values[key]
-        logger.info(f"Executing tasks for blum '{key}'...")
-        execute_tasks(key, id)
-    try:
-        bit_browser_request.windowbounds_flexable()
-    except Exception:
-        logger.error("窗口自适应排列失败")
+    # 开启几个线程
+    thread_num = 20
 
-    logger.info("All tasks completed successfully.")
+    # select = list(range(1, 41))
+    select = [1800, 393, 454, 563, 794, 1930, 19, 21, 83, 209, 269, 298, 324, 436, 455, 472, 610, 657, 704, 784, 954,
+              993, 1020, 1100, 1188, 1212, 1308, 1351, 1367, 1398, 1480, 1553, 1554, 1630, 1723, 1836, 1988, 1989, 2104,
+              2216, 2254, 2281, 2346, 2442, 2449, 2482, 2494, 2499]
+
+    create_threads(thread_num, select)
--- kill_bit.py
+++ kill_bit.py
@@ -1,23 +1,23 @@
-import os
-import signal
+import psutil
 
 from log_config import setup_logger
 
 logger = setup_logger('kill_thread', 'blum_auto.log')
 
-
 def terminate_processes(pids):
     for pid in pids:
         try:
-            os.kill(pid, signal.SIGTERM)  # 或者使用 signal.SIGKILL 直接强制终止
+            process = psutil.Process(pid)
+            process.terminate()
             logger.info(f"进程 {pid} 已成功终止")
-        except ProcessLookupError:
+        except psutil.NoSuchProcess:
             logger.info(f"没有找到 PID 为 {pid} 的进程")
-        except PermissionError:
+        except psutil.AccessDenied:
             logger.info(f"权限不足，无法终止进程 {pid}")
-
+        except Exception as e:
+            logger.error(f"终止进程 {pid} 时发生错误: {str(e)}")
 
 if __name__ == '__main__':
     # 示例：调用方法并传入 PID 列表
-    pid_list = [31972]  # 将此替换为目标进程的 PID 列表
+    pid_list = [22824]  # 将此替换为目标进程的 PID 列表
     terminate_processes(pid_list)
